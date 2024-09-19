d55b0f0c07e0ab4488d6560411bed54f77121c24
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-19 13:36:52 +0300

    updated: view binding example

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlinviewbindingexample"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlinviewbindingexample"
         minSdk = 24
--- app/src/main/java/com/ismailmesutmujde/kotlinviewbindingexample/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinviewbindingexample/MainActivity.kt
@@ -5,11 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.ismailmesutmujde.kotlinviewbindingexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.buttonMake.setOnClickListener {
+            Snackbar.make(it, "Hello", Snackbar.LENGTH_SHORT).show()
+        }
 
     }
 }
